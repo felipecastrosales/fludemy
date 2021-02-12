ed2db168eb8d0ad2ca6f78b3ae14007a3641acee
Felipe Sales <[soufeliposales@gmail.com]>
2021-02-12 11:44:19 -0300

    Creating the AdvantagesSection for Web, Tablet and Mobile

---
+++ lib/pages/home/widgets/sections/advantages_section.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class AdvantagesSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget buildAdvantage(IconData iconData, String title, String subtitle) {
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(iconData, color: Colors.grey[50], size: 48),
+          SizedBox(width: 8),
+          Column(
+            children: [
+              Text(
+                title, 
+                style: TextStyle(
+                  fontSize: 16, 
+                  fontWeight: FontWeight.w800, 
+                  color: Colors.grey[50],
+                  letterSpacing: 1.1,
+                  height: 0.4,
+                ),
+              ),
+              Text(
+                subtitle, 
+                style: TextStyle(
+                  fontSize: 14, 
+                  fontWeight: FontWeight.w600, 
+                  color: Colors.grey[50],
+                  letterSpacing: 1.2,
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 24),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey[700])),
+      ),
+      child: Wrap(
+        alignment: WrapAlignment.spaceEvenly,
+        runSpacing: 16,
+        spacing: 16,
+        children: [
+          buildAdvantage(
+            Icons.connect_without_contact, '+100.000 developers\n', 'Awesome!'),
+          buildAdvantage(
+            Icons.card_membership, 'Certificate of Complete\n', 'Sensational!'),
+          buildAdvantage(
+            Icons.verified, 'Full Access\n', 'Anywhere!'),
+        ],
+      ),
+    );
+  }
+}
